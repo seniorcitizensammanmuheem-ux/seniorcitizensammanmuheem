22e63bd09a2f36730dad326060b5f1a1255603a3
unknown <kazialim12@gmail.com>
2026-05-11 10:12:32 +0530

    fix: add react plugin to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,17 @@ import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { nitro } from "nitro/vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     tsConfigPaths(),
     tanstackStart(),
     nitro({
       preset: "vercel",
     }),
+    react(),
   ],
 });
