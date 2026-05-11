9afff90ee368bc9972fe4b495a56ad31c56966f8
unknown <kazialim12@gmail.com>
2026-05-11 09:28:25 +0530

    fix: nitro vercel preset working

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   plugins: [
     tsConfigPaths(),
     tanstackStart(),
+    nitro({
+      preset: "vercel",
+    }),
   ],
 });
